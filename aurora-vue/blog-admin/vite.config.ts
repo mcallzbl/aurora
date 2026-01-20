bf348df962656b72ec2550d693194366b84db616
mcallzbl <mcallzbl@qq.com>
2026-01-20 22:59:22 +0800

    反向代理

--- aurora-vue/blog-admin/vite.config.ts
+++ aurora-vue/blog-admin/vite.config.ts
@@ -18,4 +18,13 @@ export default defineConfig(({ command }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://blogmgr.devillusion.asia/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 }))
